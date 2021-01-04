22f4f8d3a9b511637a86552182beb4e70b1d09e5
liamattard <liam.dratta@gmail.com>
2021-01-04 21:28:33 +0100

    Added Dataset

---
+++ Project/main.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MachineLearningICS3206',
+      version='1.0',
+      description='ICS3206 Assignment',
+      author='Liam Attard',
+      author_email='liam.attard.18@um.edu.mt',
+      packages=['distutils', 'distutils.command'],
+     )
